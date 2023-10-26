dbaa1038d6def218940c428e8e503068934f0092
artur10563 <galkaartur2004galkaartur@gmail.com>
2023-10-26 19:34:05 +0300

    ScrollToTop button (js)

---
+++ E-SHOP.UI/wwwroot/js/scrollToTop.js
@@ -0,0 +1,21 @@
+﻿function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+let scrollUpButton = document.getElementById('scrollUpButton');
+let isScrolling = false;
+
+window.addEventListener('scroll', function () {
+    if (!isScrolling) {
+        isScrolling = true;
+        scrollUpButton.style.display = 'block';
+
+        setTimeout(function () {
+            isScrolling = false;
+            scrollUpButton.style.display = 'none';
+        }, 2000);
+    }
+});
+
+scrollUpButton.addEventListener('click', scrollToTop);
+
