aa552cd9ed82e0a0591cf324cb20a0244b4023fe
artur10563 <galkaartur2004galkaartur@gmail.com>
2023-11-07 20:36:22 +0200

    Register page. No EMail validation yet

--- IdentityServer/HostingExtensions.cs
+++ IdentityServer/HostingExtensions.cs
@@ -2,6 +2,7 @@ using Duende.IdentityServer;
 using IdentityServer.Data;
 using IdentityServer.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -10,65 +11,65 @@ namespace IdentityServer;
 
 internal static class HostingExtensions
 {
-    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
-    {
-        builder.Services.AddRazorPages();
+	public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+	{
+		builder.Services.AddRazorPages();
 
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+		builder.Services.AddDbContext<ApplicationDbContext>(options =>
+			options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-        builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-            .AddEntityFrameworkStores<ApplicationDbContext>()
-            .AddDefaultTokenProviders();
+		builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+			.AddEntityFrameworkStores<ApplicationDbContext>()
+			.AddDefaultTokenProviders();
 
-        builder.Services.AddIdentityServer(options =>
-        {
-            options.Events.RaiseErrorEvents = true;
-            options.Events.RaiseInformationEvents = true;
-            options.Events.RaiseFailureEvents = true;
-            options.Events.RaiseSuccessEvents = true;
+		builder.Services.AddIdentityServer(options =>
+		{
+			options.Events.RaiseErrorEvents = true;
+			options.Events.RaiseInformationEvents = true;
+			options.Events.RaiseFailureEvents = true;
+			options.Events.RaiseSuccessEvents = true;
 
-        })
-            .AddAspNetIdentity<ApplicationUser>()
-            .AddInMemoryIdentityResources(Config.IdentityResources)
-            .AddInMemoryApiScopes(Config.ApiScopes)
-            .AddInMemoryClients(Config.Clients)
-            .AddDeveloperSigningCredential();
+		})
+			.AddAspNetIdentity<ApplicationUser>()
+			.AddInMemoryIdentityResources(Config.IdentityResources)
+			.AddInMemoryApiScopes(Config.ApiScopes)
+			.AddInMemoryClients(Config.Clients)
+			.AddDeveloperSigningCredential();
 
 
 
-        builder.Services.AddAuthentication()
-            .AddGoogle(options =>
-            {
-                options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+		builder.Services.AddAuthentication()
+			.AddGoogle(options =>
+			{
+				options.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
 
-                // register your IdentityServer with Google at https://console.developers.google.com
-                // enable the Google+ API
-                // set the redirect URI to https://localhost:5001/signin-google
-                options.ClientId = "copy client ID from Google here";
-                options.ClientSecret = "copy client secret from Google here";
-            });
+				// register your IdentityServer with Google at https://console.developers.google.com
+				// enable the Google+ API
+				// set the redirect URI to https://localhost:5001/signin-google
+				options.ClientId = "copy client ID from Google here";
+				options.ClientSecret = "copy client secret from Google here";
+			});
 
-        return builder.Build();
-    }
+		return builder.Build();
+	}
 
-    public static WebApplication ConfigurePipeline(this WebApplication app)
-    {
-        app.UseSerilogRequestLogging();
+	public static WebApplication ConfigurePipeline(this WebApplication app)
+	{
+		app.UseSerilogRequestLogging();
 
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-        }
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseDeveloperExceptionPage();
+		}
 
-        app.UseStaticFiles();
-        app.UseRouting();
-        app.UseIdentityServer();
-        app.UseAuthorization();
+		app.UseStaticFiles();
+		app.UseRouting();
+		app.UseIdentityServer();
+		app.UseAuthorization();
 
-        app.MapRazorPages()
-            .RequireAuthorization();
+		app.MapRazorPages()
+			.RequireAuthorization();
 
-        return app;
-    }
+		return app;
+	}
 }
