4eb1f97f4873b10305ceae4a4e265c8bf9dd9c13
artur10563 <galkaartur2004galkaartur@gmail.com>
2023-11-21 21:43:53 +0200

    Fixed profile edit and changed allowed phone length

---
+++ OnePrice.Infrastructure/Migrations/20231121193533_PhoneNumber_Length12.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnePrice.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PhoneNumber_Length12 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Users",
+                type: "nvarchar(12)",
+                maxLength: 12,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Users",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(12)",
+                oldMaxLength: 12,
+                oldNullable: true);
+        }
+    }
+}
