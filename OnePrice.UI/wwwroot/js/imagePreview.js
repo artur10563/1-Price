70c54dda44e479f64ae8b5786f0cf6ac5e55bb39
artur10563 <galkaartur2004galkaartur@gmail.com>
2023-11-25 11:41:54 +0200

    Image preview fix

--- OnePrice.UI/wwwroot/js/imagePreview.js
+++ OnePrice.UI/wwwroot/js/imagePreview.js
@@ -1,26 +1,24 @@
-﻿
-function initializeImagePreview(elementId) {
-    const elementsWithStyle = $('#' + elementId);
+﻿document.getElementById('fileInput').addEventListener('change', function () {
+    handleFileChange(this, 'uploadedImage');
+});
 
-    elementsWithStyle.on('change', function () {
-        const file = this.files[0];
-        const promptText = $(this).siblings('.prompt-text');
-        const uploadedImage = $(this);
+document.getElementById('fileInputCreate').addEventListener('change', function () {
+    handleFileChange(this, 'uploadedImageCreate');
+});
 
-        if (file && file.size <= 5 * 1024 * 1024 && file.type.startsWith('image/')) {
-            const reader = new FileReader();
-            reader.onload = function () {
-                uploadedImage.attr('src', reader.result);
-                promptText.hide();
-            };
-            reader.readAsDataURL(file);
-        } else {
-            alert("Image size must be 5mb or less!");
-        }
-    });
+function handleFileChange(input, imageId) {
+    const file = input.files[0];
+    const promptText = document.querySelector('.prompt-text');
+    const uploadedImage = document.getElementById(imageId);
+
+    if (file && file.size <= 5 * 1024 * 1024 && file.type.startsWith('image/')) {
+        const reader = new FileReader();
+        reader.onload = function () {
+            uploadedImage.src = reader.result;
+            promptText.style.display = 'none';
+        };
+        reader.readAsDataURL(file);
+    } else {
+        alert("Image size must be 5mb or less!");
+    }
 }
-
-initializeImagePreview('uploadedImageCreate');
-initializeImagePreview('uploadedImage');
-initializeImagePreview('uploadedImg');
-initializeImagePreview('uploadedImageEdit');
