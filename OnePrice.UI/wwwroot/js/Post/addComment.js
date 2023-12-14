1a78ac733e879e0da9e82e773f413815c0df4f7c
artur10563 <galkaartur2004galkaartur@gmail.com>
2023-12-14 23:45:42 +0200

    Separated JS2

---
+++ OnePrice.UI/wwwroot/js/Post/addComment.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    $("#submitCommentBtn").click(function () {
+        var commentText = $("#commentInput").val();
+        var postId = $('#Id').val();
+
+        var commentData = {
+            PostId: postId,
+            Content: commentText
+        };
+        $.ajax({
+            url: '/Comment/AddComment',
+            type: 'POST',
+            data: commentData,
+
+            success: function (result) {
+                $("#commentsContainer").prepend(result);
+                $("#commentInput").val('');
+                $("#validationErrors").empty();
+            },
+
+            error: function (xhr) {
+                if (xhr.responseJSON && xhr.responseJSON.errors) {
+                    var errorsHtml = xhr.responseJSON.errors.map(function (error) {
+                        return '<p>' + error + '</p>';
+                    }).join('');
+                    $("#validationErrors").html(errorsHtml);
+                } else {
+                    $("#validationErrors").html("Could not post a comment. Try again later.");
+                }
+            }
+
+        });
+    });
+});
---
+++ OnePrice.UI/wwwroot/js/Post/showContacts.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $('#showContacts').click(function () {
+        var phoneNumber = $(this).data('phone');
+
+        if (phoneNumber !== undefined && phoneNumber !== null && phoneNumber.toString().length > 0) {
+            $('#showContacts').html(phoneNumber);
+        } else {
+            $('#showContacts').html('Phone number is not available');
+        }
+    });
+});
